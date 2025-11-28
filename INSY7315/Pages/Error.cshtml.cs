ac76c6d7882c45d1d3c4b65d28417314885d2cbc
ST10330722 <ST10330722@vcconnect.edu.za>
2025-11-28 16:54:50 +0200

    removed old

--- INSY7315/Pages/Error.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace INSY7315.Pages
-{
-    public class ErrorModel : PageModel
-    {
-        public void OnGet() { }
-    }
-}
--- INSY7315/Pages/Privacy.cshtml.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace INSY7315.Pages
-{
-    public class PrivacyModel : PageModel
-    {
-        public void OnGet() { }
-    }
-}
